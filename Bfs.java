5915641cbee08d2e5dcb8e4f864b17b59c42d277
Sudeep Toppo <sudeeptoppo27@email.com>
2025-06-01 04:27:33 +0530

    final

--- Bfs.java
+++ Bfs.java
@@ -1,7 +1,9 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.ArrayList;
 
-class Bfs {
-    class Edge {
+public class Bfs {
+    static class Edge {
         int src;
         int dest;
 
@@ -11,13 +13,52 @@ class Bfs {
         }
     }
 
-    public static void main(String args[]) {
-        int v = 5;
-        ArrayList<Edge>[] graph = new ArrayList[v];
-
+    public static void createGraph(ArrayList<Edge> graph[]) {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
+
+        graph[0].add(new Edge(0, 1));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 2));
+
+        graph[4].add(new Edge(4, 2));
+    }
+
+    public static void bfs(ArrayList<Edge> graph[]) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean visited[] = new boolean[graph.length];
+
+        q.add(0);
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if (!visited[curr]) {
+                System.out.print(curr + " ");
+                visited[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int v = 5;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        createGraph(graph);
+        bfs(graph);
 
     }
 }
